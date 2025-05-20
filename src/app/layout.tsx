2b041117b2dcfe1a4c2cba14548f6742f8691db2
Mohammadpvn1996 <mohammad.pvn1375@gmail.com>
2025-05-20 10:02:28 +0330

    revert unused changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import type React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -7,11 +6,11 @@ import { store } from "@/lib/store";
 import { Provider as ReduxProvider } from "react-redux";
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata = {
-//   title: "Salona App",
-//   description: "Salona application with React, Tailwind, and Ant Design",
-//     generator: 'v0.dev'
-// }
+export const metadata = {
+  title: "Salona App",
+  description: "Salona application with React, Tailwind, and Ant Design",
+    generator: 'v0.dev'
+}
 
 export default function RootLayout({
   children,
